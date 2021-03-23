faafb368cbd9f992955495273bb197305e27d191
The_philosopher <shinudonney@tuta.io>
2021-03-23 22:45:20 +0530

    Add barrel file for auth/decorators

--- src/loans/loans.controller.ts
+++ src/loans/loans.controller.ts
@@ -14,8 +14,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from 'src/auth/decorators/get-user.decorator';
-import { Roles } from 'src/auth/decorators/roles.decorator';
+import { GetUser, Roles } from 'src/auth/decorators';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UserRole } from 'src/auth/user-role.enum';
 import { User } from 'src/auth/user.entity';
