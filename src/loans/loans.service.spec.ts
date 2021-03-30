3e788217b7d6a0d30f2dfce492f5728ee7e5f6ca
The_philosopher <shinudonney@tuta.io>
2021-03-30 13:36:55 +0530

    Add unit tests for update loan request status and delete loan request

--- src/loans/loans.service.spec.ts
+++ src/loans/loans.service.spec.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { UserRole } from 'src/auth/user-role.enum';
 import { User } from 'src/auth/user.entity';
@@ -11,6 +11,7 @@ const mockLoanRepository = () => ({
   getLoanRequests: jest.fn(),
   findOne: jest.fn(),
   createLoanRequest: jest.fn(),
+  delete: jest.fn(),
 });
 
 const mockUserRepository = () => ({
@@ -66,14 +67,15 @@ describe('LoansService', () => {
   });
 
   describe('getLoanRequestById', () => {
+    const loanRequestId = 1;
     it("should pass a custom findOne() argument that only filters through the customer's loan requests", async () => {
       mockUser.role = UserRole.CUSTOMER;
       loanRepository.findOne.mockResolvedValue('mock_user');
 
       expect(loanRepository.findOne).not.toHaveBeenCalled();
-      await loansService.getLoanRequestById(1, mockUser);
+      await loansService.getLoanRequestById(loanRequestId, mockUser);
       expect(loanRepository.findOne).toHaveBeenCalledWith({
-        where: { loan_id: 1, customer_id: mockUser.id },
+        where: { loan_id: loanRequestId, customer_id: mockUser.id },
       });
     });
 
@@ -82,8 +84,8 @@ describe('LoansService', () => {
       loanRepository.findOne.mockResolvedValue('mock_user');
 
       expect(loanRepository.findOne).not.toHaveBeenCalled();
-      await loansService.getLoanRequestById(1, mockUser);
-      expect(loanRepository.findOne).toHaveBeenCalledWith(1);
+      await loansService.getLoanRequestById(loanRequestId, mockUser);
+      expect(loanRepository.findOne).toHaveBeenCalledWith(loanRequestId);
     });
 
     it('should throw an NotFoundException if loan request is not found', async () => {
@@ -92,7 +94,7 @@ describe('LoansService', () => {
 
       expect(loanRepository.findOne).not.toHaveBeenCalled();
       await expect(
-        loansService.getLoanRequestById(1, mockUser),
+        loansService.getLoanRequestById(loanRequestId, mockUser),
       ).rejects.toThrow(NotFoundException);
     });
 
@@ -100,7 +102,10 @@ describe('LoansService', () => {
       mockUser.role = UserRole.ADMIN;
       loanRepository.findOne.mockResolvedValue('mock_user');
 
-      const result = await loansService.getLoanRequestById(1, mockUser);
+      const result = await loansService.getLoanRequestById(
+        loanRequestId,
+        mockUser,
+      );
       expect(result).toEqual('mock_user');
       expect(result).not.toEqual('something_else');
     });
@@ -147,6 +152,62 @@ describe('LoansService', () => {
         mockUser,
       );
       expect(result).toEqual('new_loan_request');
+    });
+  });
+
+  describe('UpdateLoanRequestStatus', () => {
+    const loanRequestId = 1;
+
+    it('should update loan status', async () => {
+      const save = jest.fn().mockResolvedValue(true);
+      loansService.getLoanRequestById = jest.fn().mockResolvedValue({
+        loan_status: LoanStatus.NEW,
+        save,
+      });
+
+      const result = await loansService.updateLoanRequestStatus(
+        loanRequestId,
+        LoanStatus.APPROVED,
+        mockUser,
+      );
+      expect(result.loan_status).toEqual(LoanStatus.APPROVED);
+    });
+  });
+
+  describe('deleteLoanRequest', () => {
+    const loanRequestId = 1;
+
+    it('should throw a NotFoundException if loan request is not found', async () => {
+      loanRepository.findOne.mockResolvedValue(undefined);
+      await expect(
+        loansService.deleteLoanRequest(loanRequestId),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw an UnauthorizedException if loan request is approved', async () => {
+      loanRepository.findOne.mockResolvedValue({
+        loan_status: LoanStatus.APPROVED,
+      });
+      await expect(
+        loansService.deleteLoanRequest(loanRequestId),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should throw an UnauthorizedException if loan request is new', async () => {
+      loanRepository.findOne.mockResolvedValue({
+        loan_status: LoanStatus.NEW,
+      });
+      await expect(
+        loansService.deleteLoanRequest(loanRequestId),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should call loanRepository.delete() if loan request is found and is rejected', async () => {
+      loanRepository.findOne.mockResolvedValue({
+        loan_status: LoanStatus.REJECTED,
+      });
+      await loansService.deleteLoanRequest(loanRequestId);
+      expect(loanRepository.delete).toHaveBeenCalledWith(loanRequestId);
     });
   });
 });
