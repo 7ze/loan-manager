cb16fec71baa282213872fb128b0cbe25b6e024b
The_philosopher <shinudonney@tuta.io>
2021-03-30 11:19:34 +0530

    Add unit test for get request by id

--- src/loans/loans.service.spec.ts
+++ src/loans/loans.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { UserRole } from 'src/auth/user-role.enum';
 import { User } from 'src/auth/user.entity';
 import { UserRepository } from 'src/auth/user.repository';
 import { LoanStatus } from './loan-status.enum';
@@ -7,6 +9,7 @@ import { LoansService } from './loans.service';
 
 const mockLoanRepository = () => ({
   getLoanRequests: jest.fn(),
+  findOne: jest.fn(),
 });
 
 const mockUserRepository = () => ({
@@ -41,7 +44,7 @@ describe('LoansService', () => {
   });
 
   describe('getLoanRequests', () => {
-    it('should be a pure function that returns the result of loanRepository.getLoanRequests()', async () => {
+    it('should return the result of loanRepository.getLoanRequests() unchanged', async () => {
       loanRepository.getLoanRequests.mockResolvedValue('loan_requests');
       const mockLoanRequestFilterDto = {
         status: LoanStatus.NEW,
@@ -58,6 +61,47 @@ describe('LoansService', () => {
       );
       expect(result).toEqual('loan_requests');
       expect(result).not.toEqual('some_modified_result');
+    });
+  });
+
+  describe('getLoanRequestById', () => {
+    it("should pass a custom findOne() argument that only filters through the customer's loan requests", async () => {
+      mockUser.role = UserRole.CUSTOMER;
+      loanRepository.findOne.mockResolvedValue('mock_user');
+
+      expect(loanRepository.findOne).not.toHaveBeenCalled();
+      await loansService.getLoanRequestById(1, mockUser);
+      expect(loanRepository.findOne).toHaveBeenCalledWith({
+        where: { loan_id: 1, customer_id: mockUser.id },
+      });
+    });
+
+    it('should do the regular findOne() if user is not a customer', async () => {
+      mockUser.role = UserRole.ADMIN;
+      loanRepository.findOne.mockResolvedValue('mock_user');
+
+      expect(loanRepository.findOne).not.toHaveBeenCalled();
+      await loansService.getLoanRequestById(1, mockUser);
+      expect(loanRepository.findOne).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw an NotFoundException if loan request is not found', async () => {
+      mockUser.role = UserRole.CUSTOMER;
+      loanRepository.findOne.mockResolvedValue(undefined);
+
+      expect(loanRepository.findOne).not.toHaveBeenCalled();
+      await expect(
+        loansService.getLoanRequestById(1, mockUser),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should return the result of loanRepository.findOne() unchanged if loan request is found', async () => {
+      mockUser.role = UserRole.ADMIN;
+      loanRepository.findOne.mockResolvedValue('mock_user');
+
+      const result = await loansService.getLoanRequestById(1, mockUser);
+      expect(result).toEqual('mock_user');
+      expect(result).not.toEqual('something_else');
     });
   });
 });
